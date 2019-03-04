aa81d0a11a4b1f03979d0803c30cf855d7753c0a
Mvcampbell3 <mvcampbell3@gmail.com>
2019-03-03 21:33:36 -0500

    updating jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 const mysql = require("mysql");
+const con;
 
-const con = mysql.createConnection({
-    port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "surfboard",
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    con = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    con = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "surfboard",
+        database: "burgers_db"
+    })
+}
+
 
 con.connect((err) => {
     if (err) throw err;
