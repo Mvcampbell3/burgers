637aeeb63332d61f6833d292463ba67973cf68ef
Mvcampbell3 <mvcampbell3@gmail.com>
2019-03-06 20:03:49 -0500

    added mobile media queries

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,4 +1,4 @@
-$(function() {
+$(function () {
     $(".addBtn").on("click", function () {
         let newName = {
             name: $("#burgerName").val().trim()
@@ -7,24 +7,24 @@ $(function() {
         $.ajax("/api/burger", {
             type: "POST",
             data: newName
-        }).then(function(){
+        }).then(function () {
             console.log("added new burger");
             location.reload();
         })
     })
 
-    $(".deleteBtn").on("click", function() {
+    $(".deleteBtn").on("click", function () {
         console.log($(this).attr("data-which"));
         let id = $(this).attr("data-which");
         $.ajax("/api/burger/" + id, {
             type: "DELETE"
-        }).then(function() {
+        }).then(function () {
             console.log("deleted burger");
             location.reload();
         })
     })
 
-    $(".devourBtn").on("click", function() {
+    $(".devourBtn").on("click", function () {
         let id = $(this).attr("data-which");
         console.log(id);
         let devour = $(this).attr("data-newDevour");
@@ -36,12 +36,12 @@ $(function() {
         $.ajax("/api/burger/" + id, {
             type: "PUT",
             data: newDevour
-        }).then(function(result) {
+        }).then(function (result) {
             console.log(result)
             // add reload
             location.reload();
         })
     })
 
-    
+
 })
